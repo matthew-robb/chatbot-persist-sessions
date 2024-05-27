457721bb207185c52b6d3a51a1f4862716ebd85d
Matthew Robb <matthewrobb16@gmail.com>
2024-05-27 23:15:55 +0100

    fixed needless re-initialization of connections

--- src/chatbot_persist_sessions/main.py
+++ src/chatbot_persist_sessions/main.py
@@ -1,12 +1,21 @@
-
 import uuid
-from typing import Annotated
-from fastapi import FastAPI, Body, Depends
-from langchain_core.language_models import BaseChatModel
-from psycopg import Connection, AsyncConnection
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Body
 from starlette.responses import StreamingResponse
 
 from .utils import fetch_chain, fetch_connection, fetch_async_connection, chunk_processor, fetch_llm
+
+dependencies = {}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    dependencies["llm"] = fetch_llm
+    dependencies["sync_conn"] = fetch_connection
+    dependencies["async_conn"] = fetch_async_connection
+    yield dependencies
+    dependencies["sync_conn"].close()
+    dependencies["async_conn"].close()
+
+
 
 app = FastAPI()
 
@@ -14,12 +23,9 @@ app = FastAPI()
 @app.post("/chat/{session_id}")
 async def chat(
     session_id: str,
-    llm: Annotated[BaseChatModel, Depends(fetch_llm)],
-    sync_conn: Annotated[Connection, Depends(fetch_connection)],
-    async_conn: Annotated[AsyncConnection, Depends(fetch_async_connection)],
     message: str = Body(..., media_type="text"),
 ) -> StreamingResponse:
-    chain = await fetch_chain(session_id, llm, sync_conn, async_conn)
+    chain = await fetch_chain(session_id, dependencies['llm'], dependencies['sync_conn'], dependencies['async_conn'])
     return StreamingResponse(
         chunk_processor(chain.astream_log(input=message)),
         media_type="text/event-stream",
