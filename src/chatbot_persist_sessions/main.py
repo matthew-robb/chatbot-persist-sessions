ccf7ff1dcc19023477ff36b6fec63adcd6a05443
Matthew Robb <matthewrobb16@gmail.com>
2024-05-27 23:25:46 +0100

    fixed needless re-initialization of connections

--- src/chatbot_persist_sessions/main.py
+++ src/chatbot_persist_sessions/main.py
@@ -6,18 +6,19 @@ from starlette.responses import StreamingResponse
 from .utils import fetch_chain, fetch_connection, fetch_async_connection, chunk_processor, fetch_llm
 
 dependencies = {}
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    dependencies["llm"] = fetch_llm
-    dependencies["sync_conn"] = fetch_connection
-    dependencies["async_conn"] = fetch_async_connection
+    dependencies["llm"] = fetch_llm()
+    dependencies["sync_conn"] = fetch_connection()
+    dependencies["async_conn"] = await fetch_async_connection()
     yield dependencies
     dependencies["sync_conn"].close()
     dependencies["async_conn"].close()
 
 
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/chat/{session_id}")
@@ -25,7 +26,7 @@ async def chat(
     session_id: str,
     message: str = Body(..., media_type="text"),
 ) -> StreamingResponse:
-    chain = await fetch_chain(session_id, dependencies['llm'], dependencies['sync_conn'], dependencies['async_conn'])
+    chain = await fetch_chain(session_id, dependencies["llm"], dependencies["sync_conn"], dependencies["async_conn"])
     return StreamingResponse(
         chunk_processor(chain.astream_log(input=message)),
         media_type="text/event-stream",
